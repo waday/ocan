7f05c6ca8171578afa836fc8fc18c792bb495662
waday <amwtpgaj@gmail.com>
2014-11-15 21:17:24 +0900

    rename proto.rb to ocan.rb

--- ocan.rb
+++ ocan.rb
@@ -5,17 +5,17 @@ def prompt(gizmo)
   return gizmo.name + ':' + gizmo.responder_name + '(' + gizmo.emotion.mood.to_s + ')> '
 end
 
-puts('Unmo System prototype : proto')
-proto = Gizmo.new('proto')
+puts('Unmo System prototype : ocan')
+ocan = Gizmo.new('ocan')
 while true
   print('> ')
   input = gets
   input.chomp!
   if input == ''
-    proto.save
+    ocan.save
     break
   end
 
-  response = proto.dialogue(input)
-  puts(prompt(proto) + response)
+  response = ocan.dialogue(input)
+  puts(prompt(ocan) + response)
 end
